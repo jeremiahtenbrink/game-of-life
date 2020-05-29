fd3f5c8a94cc0af1c2ade765b35ff2db18549e47
jeremiahtenbrink <jeremiahtenbrink@gmail.com>
2020-05-28 19:02:34 -0600

    Updates

---
+++ precache-manifest.76aceeeec6084d8f9f54421269ce5a35.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8d34c2b1369335a6e22f0fb2a33d86e",
+    "url": "/game-of-life/index.html"
+  },
+  {
+    "revision": "5be0a726b0c1e5da6203",
+    "url": "/game-of-life/static/css/2.63b65d4e.chunk.css"
+  },
+  {
+    "revision": "72bfbe5b5465366f1986",
+    "url": "/game-of-life/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5be0a726b0c1e5da6203",
+    "url": "/game-of-life/static/js/2.6894f209.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/game-of-life/static/js/2.6894f209.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72bfbe5b5465366f1986",
+    "url": "/game-of-life/static/js/main.e2103a5b.chunk.js"
+  },
+  {
+    "revision": "74c0ec62b28c760df103",
+    "url": "/game-of-life/static/js/runtime-main.ccc8ab68.js"
+  }
+]);
